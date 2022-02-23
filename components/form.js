df2fb8ddf2cdbdf0af9506eab9b1bfac84edd4cf
unknown <kavita.kaur998@gmail.com>
2022-02-23 01:07:06 +0000

    form tbc and post request to be made after modal to confirm

--- components/form.js
+++ components/form.js
@@ -1,37 +1,110 @@
-import { FormControl, FormLabel, Button, Input } from "@chakra-ui/react";
-import { Formik } from "formik";
+import {
+  // Form,
+  Select,
+  FormControl,
+  FormLabel,
+  Button,
+  Input,
+  Box,
+  Editable,
+  EditableInput,
+  EditablePreview,
+} from "@chakra-ui/react";
+import { EditIcon } from "@chakra-ui/icons";
+import { useForm, Controller } from "react-hook-form";
 import DatePicker from "react-datepicker";
 import React, { useState } from "react";
 import "react-datepicker/dist/react-datepicker.css";
-
+import { SingleDatepicker } from "chakra-dayzed-datepicker";
 export default function CreateEventForm() {
   const [startDate, setStartDate] = useState(new Date());
+  const {
+    handleSubmit,
+    register,
+    control,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm();
+
+  function onSubmit(values, e) {
+    console.log(values);
+    e.target.reset();
+    // send data to API
+    // confirm with modal
+    // clear the form fields
+  }
+
   return (
-    <>
-      <Formik
-        initialValues={{
-          event_name: "",
-          event_description: "",
-          location: "",
-        }}
-        onSubmit={async (values) => {
-          await new Promise((r) => setTimeout(r, 500));
-          alert(JSON.stringify(values, null, 2));
-        }}
-      ></Formik>
-      <FormControl isRequired>
-        {/* event type */}
-        <Button colorScheme="teal">Code Collab</Button>{" "}
-        <Button colorScheme="teal">Hackathon</Button>
-        {/* event title */}
-        <FormLabel htmlFor="title">Event Title</FormLabel>
-        <Input id="title" placeholder="First name" />
-        {/* event location */}
-        <FormLabel htmlFor="location">Event Location</FormLabel>
-        <Input id="location" placeholder="First name" />
-        {/* calander */}
-        <FormLabel htmlFor="date">Event Date</FormLabel>
-        <DatePicker
+    <Box p={12}>
+      {/* event type */}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormLabel htmlFor="type">Event type</FormLabel>
+        <Select
+          placeholder="Select the type of event"
+          {...register("Type", {
+            required: "This is required",
+            minLength: {
+              value: 4,
+              message: "Plase provide a link to your event",
+            },
+          })}
+        >
+          <option value="Hackathon">Hackathon</option>
+          <option value="Code Club">Code Club</option>
+        </Select>
+        <FormLabel htmlFor="name">Description</FormLabel>
+        <Input id="Description" {...register("title")} />
+        <FormLabel htmlFor="Location">Meeting URL</FormLabel>
+        <Input
+          id="Location"
+          {...register("Location", {
+            required: "This is required",
+            minLength: {
+              value: 4,
+              message: "Plase provide a link to your event",
+            },
+          })}
+        />
+        <FormLabel>Date</FormLabel>
+        <Controller
+          control={control}
+          name="SingleDatepicker"
+          render={({ field }) => (
+            <SingleDatepicker
+              placeholderText="Select date"
+              onDateChange={(e) => field.onChange(e)}
+              selected={field.value}
+            />
+          )}
+        />
+        {/* event duration */}
+        <p> insert duration here</p>
+        {/* tags  */}
+        <FormLabel>
+          <EditIcon /> Provide three tags to help describe your event
+        </FormLabel>
+        <Editable defaultValue="Tag 1" width="200px" {...register("Editable")}>
+          <EditablePreview />
+          <EditableInput />
+        </Editable>
+        <Editable defaultValue="Tag 2" width="200px">
+          <EditablePreview />
+          <EditableInput {...register("EditableInput")} />
+        </Editable>
+        <Editable defaultValue="Tag 3" width="200px">
+          <EditablePreview {...register("Editablep")} />
+          <EditableInput />
+        </Editable>
+        <Button width="200px" mt={4} type="submit" isLoading={isSubmitting}>
+          Submit
+        </Button>
+      </form>
+    </Box>
+  );
+}
+
+{
+  /* <DatePicker
           selected={startDate}
           onChange={(date) => setStartDate(date)}
           showTimeSelect
@@ -39,14 +112,5 @@ export default function CreateEventForm() {
           timeIntervals={30}
           timeCaption="time"
           dateFormat="MMMM d, yyyy h:mm aa"
-        />
-        {/* event duration */}
-        <p> insert event length here</p>
-        {/* tags  */}
-        <Button colorScheme="blue">tag1</Button>{" "}
-        <Button colorScheme="blue">tag2</Button>{" "}
-        <Button colorScheme="blue">tag3</Button>
-      </FormControl>
-    </>
-  );
+        /> */
 }
