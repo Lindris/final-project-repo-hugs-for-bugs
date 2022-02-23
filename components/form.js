6d9eb48ec8aba6e3c2aa0e6c504c76016fe8991b
unknown <kavita.kaur998@gmail.com>
2022-02-23 10:47:17 +0000

    modal and form to be formatted

--- components/form.js
+++ components/form.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import {
   // Form,
   Select,
@@ -8,15 +9,24 @@ import {
   Editable,
   EditableInput,
   EditablePreview,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
 } from "@chakra-ui/react";
 import { EditIcon } from "@chakra-ui/icons";
 import { useForm, Controller } from "react-hook-form";
 import DatePicker from "react-datepicker";
-import React, { useState } from "react";
 import "react-datepicker/dist/react-datepicker.css";
 
 export default function CreateEventForm() {
-  const [startDate, setStartDate] = useState(new Date());
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  // const [startDate, setStartDate] = useState(new Date());
+  const [eventdetails, setEventDetails] = useState([]);
   const {
     handleSubmit,
     register,
@@ -27,6 +37,10 @@ export default function CreateEventForm() {
 
   function onSubmit(values, e) {
     console.log(values);
+    onOpen();
+    const valuesArray = Object.entries(values);
+    console.log(valuesArray);
+    setEventDetails(valuesArray);
     // e.target.reset();
     // send data to API
     // confirm with modal
@@ -34,107 +48,129 @@ export default function CreateEventForm() {
   }
 
   return (
-    <Box p={12}>
-      {/* event type */}
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <FormLabel htmlFor="type">Event type</FormLabel>
-        <Select
-          placeholder="Select the type of event"
-          {...register("Type", {
-            required: "This is required",
-            minLength: {
-              value: 4,
-              message: "Plase provide a link to your event",
-            },
-          })}
-        >
-          <option value="Hackathon">Hackathon</option>
-          <option value="Code Club">Code Club</option>
-        </Select>
-        <FormLabel htmlFor="name">Description</FormLabel>
-        <Input id="Description" {...register("title")} />
-        <FormLabel htmlFor="Location">Meeting URL</FormLabel>
-        <Input
-          id="Location"
-          {...register("Location", {
-            required: "This is required",
-            minLength: {
-              value: 4,
-              message: "Plase provide a link to your event",
-            },
-          })}
-        />
-        <FormLabel>Date</FormLabel>
-        <Controller
-          control={control}
-          name="DatePicker"
-          render={({ field }) => (
-            <DatePicker
-              dateFormat="MMMM d, yyyy"
-              onChange={(e) => field.onChange(e)}
-              selected={field.value}
+    <>
+      <Box p={12}>
+        {/* event type */}
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormLabel htmlFor="type">Event type</FormLabel>
+          <Select
+            placeholder="Select the type of event"
+            {...register("Type", {
+              required: "This is required",
+              minLength: {
+                value: 4,
+                message: "Plase provide a link to your event",
+              },
+            })}
+          >
+            <option value="Hackathon">Hackathon</option>
+            <option value="Code Club">Code Club</option>
+          </Select>
+          <FormLabel htmlFor="name">Description</FormLabel>
+          <Input id="Description" {...register("title")} />
+          <FormLabel htmlFor="Location">Meeting URL</FormLabel>
+          <Input
+            id="Location"
+            {...register("Location", {
+              required: "This is required",
+              minLength: {
+                value: 4,
+                message: "Plase provide a link to your event",
+              },
+            })}
+          />
+          <FormLabel>Date</FormLabel>
+          {/* <Controller
+            control={control}
+            name="DatePicker"
+            render={({ field }) => (
+              <DatePicker
+                dateFormat="MMMM d, yyyy"
+                onChange={(e) => field.onChange(e)}
+                selected={field.value}
+              />
+            )}
+          /> */}
+          {/*start time range picker */}
+          {/* <FormLabel>Start Time</FormLabel>
+          <section>
+            <Controller
+              control={control}
+              name="StartTimeRange"
+              render={({ field }) => (
+                <DatePicker
+                  selected={field.value}
+                  onChange={(e) => field.onChange(e)}
+                  showTimeSelect
+                  showTimeSelectOnly
+                  timeIntervals={30}
+                  timeCaption="Time"
+                  dateFormat="h:mm aa"
+                />
+              )}
             />
-          )}
-        />
-        {/*start time range picker */}
-        <FormLabel>Start Time</FormLabel>
-        <section>
-          <Controller
-            control={control}
-            name="StartTimeRange"
-            render={({ field }) => (
-              <DatePicker
-                selected={field.value}
-                onChange={(e) => field.onChange(e)}
-                showTimeSelect
-                showTimeSelectOnly
-                timeIntervals={30}
-                timeCaption="Time"
-                dateFormat="h:mm aa"
-              />
-            )}
-          />
-        </section>
+          </section> */}
 
-        {/* end time range picker */}
-        <FormLabel>End Time</FormLabel>
-        <section>
-          <Controller
-            control={control}
-            name="EndTimeRange"
-            render={({ field }) => (
-              <DatePicker
-                selected={field.value}
-                onChange={(e) => field.onChange(e)}
-                showTimeSelect
-                showTimeSelectOnly
-                timeIntervals={30}
-                timeCaption="Time"
-                dateFormat="h:mm aa"
-              />
-            )}
-          />
-        </section>
-        {/* tags  */}
-        <FormLabel>
-          <EditIcon /> Provide three tags to help describe your event
-        </FormLabel>
-        <Editable defaultValue="Tag 1" width="200px">
-          <EditablePreview />
-          <EditableInput {...register("tag1")} />
-        </Editable>
-        <Editable defaultValue="Tag 2" width="200px">
-          <EditablePreview />
-          <EditableInput {...register("tag2")} />
-        </Editable>
-        <Editable defaultValue="Tag 3" width="200px">
-          <EditablePreview />
-          <EditableInput {...register("tag3")} />
-        </Editable>
-        <Button width="200px" mt={4} type="submit" isLoading={isSubmitting}>
-          Submit
-        </Button>
-      </form>
-    </Box>
+          {/* end time range picker */}
+          <FormLabel>End Time</FormLabel>
+          <section>
+            <Controller
+              control={control}
+              name="EndTimeRange"
+              render={({ field }) => (
+                <DatePicker
+                  selected={field.value}
+                  onChange={(e) => field.onChange(e)}
+                  showTimeSelect
+                  showTimeSelectOnly
+                  timeIntervals={30}
+                  timeCaption="Time"
+                  dateFormat="h:mm aa"
+                />
+              )}
+            />
+          </section>
+          {/* tags  */}
+          <FormLabel>
+            <EditIcon /> Provide three tags to help describe your event
+          </FormLabel>
+          <Editable defaultValue="Tag 1" width="200px">
+            <EditablePreview />
+            <EditableInput {...register("tag1")} />
+          </Editable>
+          <Editable defaultValue="Tag 2" width="200px">
+            <EditablePreview />
+            <EditableInput {...register("tag2")} />
+          </Editable>
+          <Editable defaultValue="Tag 3" width="200px">
+            <EditablePreview />
+            <EditableInput {...register("tag3")} />
+          </Editable>
+          <Button width="200px" mt={4} type="submit" isLoading={isSubmitting}>
+            Submit
+          </Button>
+        </form>
+      </Box>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Confirmation</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {eventdetails.map((item, i) => {
+              return <p>{`${item[0]}: ${item[1].toString()}`}</p>;
+            })}
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Edit details
+            </Button>
+            <Button variant="ghost">Confirm event</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
