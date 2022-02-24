647b46ba9be1d7839569551919cb3c2a0e107714
JoshuaMeah <joshmeah7@gmail.com>
2022-02-24 16:05:56 +0000

    fetched event data from backend to frontend

--- pages/events.js
+++ pages/events.js
@@ -1,4 +1,14 @@
-export default function Events() {
+import axios from "axios";
+export default function Events({ payload }) {
+  console.log(payload);
   return <p>some text here to test what the font looks like</p>;
-
+}
+export async function getServerSideProps() {
+  // Fetch data from external AP
+  const response = await fetch(`http://localhost:5000/events`);
+  const data = await response.json();
+  const payload = data.payload;
+  // console.log(data);
+  // Pass data to the page via props
+  return { props: { payload } };
 }
