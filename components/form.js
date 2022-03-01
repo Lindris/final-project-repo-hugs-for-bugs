bbb748ec453e01af665561c43a742d6b81a28c34
Hajara <hajaraiyal@gmail.com>
2022-03-01 22:28:29 +0000

    cypress testing

--- components/form.js
+++ components/form.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import {
-  // Form,
-  Select,
-  FormLabel,
-  Button,
-  Input,
-  Box,
-  Editable,
-  EditableInput,
-  EditablePreview,
-  useDisclosure,
+	// Form,
+	Select,
+	FormLabel,
+	Button,
+	Input,
+	Box,
+	Editable,
+	EditableInput,
+	EditablePreview,
+	useDisclosure,
 } from "@chakra-ui/react";
 import { EditIcon } from "@chakra-ui/icons";
 import { useForm, Controller } from "react-hook-form";
@@ -20,196 +20,216 @@ import { useUser } from "@auth0/nextjs-auth0";
 import Router from "next/router";
 import BasicModal from "../components/modal.js";
 export default function CreateEventForm() {
-  const { user } = useUser();
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  // const [startDate, setStartDate] = useState(new Date());
-  const [eventdetails, setEventDetails] = useState([]);
-  const [formValues, setFormValues] = useState("");
-  const {
-    handleSubmit,
-    register,
-    control,
-    formState: { errors, isSubmitting },
-    reset,
-  } = useForm();
+	const { user } = useUser();
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	// const [startDate, setStartDate] = useState(new Date());
+	const [eventdetails, setEventDetails] = useState([]);
+	const [formValues, setFormValues] = useState("");
+	const {
+		handleSubmit,
+		register,
+		control,
+		formState: { errors, isSubmitting },
+		reset,
+	} = useForm();
 
-  function onSubmit(values, e) {
-    Object.keys(values).map((key) => {
-      if (key === "event_date") {
-        values[key] = values[key].toString().slice(0, 15);
-      } else if (key === "event_start_time" || key === "event_end_time") {
-        values[key] = values[key].toString().slice(16, 24);
-      }
-    });
-    // console.log(values);
-    const valuesArray = Object.entries(values);
-    setEventDetails(valuesArray);
-    console.log(user);
-    values = { ...values, auth_id: user.sub };
-    console.log(values);
-    setFormValues(values);
-    onOpen();
-  }
+	function onSubmit(values, e) {
+		Object.keys(values).map((key) => {
+			if (key === "event_date") {
+				values[key] = values[key].toString().slice(0, 15);
+			} else if (key === "event_start_time" || key === "event_end_time") {
+				values[key] = values[key].toString().slice(16, 24);
+			}
+		});
+		// console.log(values);
+		const valuesArray = Object.entries(values);
+		setEventDetails(valuesArray);
+		console.log(user);
+		values = { ...values, auth_id: user.sub };
+		console.log(values);
+		setFormValues(values);
+		onOpen();
+	}
 
-  function handleModalSubmit() {
-    console.log(formValues);
-    try {
-      fetch("http://localhost:5000/events", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formValues),
-      });
-    } catch (error) {
-      console.log("User not added to event");
-    }
-    Router.reload(window.location.pathname);
-  }
+	function handleModalSubmit() {
+		console.log(formValues);
+		try {
+			fetch("http://localhost:5000/events", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(formValues),
+			});
+		} catch (error) {
+			console.log("User not added to event");
+		}
+		Router.reload(window.location.pathname);
+	}
 
-  return (
-    <>
-      <Box
-        py={4}
-        px={10}
-        // pl={10}
-        maxW="md"
-        borderWidth="1px"
-        borderRadius="lg"
-        overflow="hidden"
-        display="flex"
-        m="0 auto"
-        mt={10}
-        // align="center"
-      >
-        {/* event type */}
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <FormLabel htmlFor="type" mt={4}>
-            Event type
-          </FormLabel>
-          <Select
-            placeholder="Select the type of event"
-            {...register("event_type", {
-              required: true,
-            })}
-          >
-            <option value="Hackathon">Hackathon</option>
-            <option value="Code Club">Code Club</option>
-          </Select>
-          <FormLabel htmlFor="name" mt={4}>
-            Description
-          </FormLabel>
-          <Input
-            id="Description"
-            {...register("event_desc", {
-              required: true,
-            })}
-          />
-          <FormLabel htmlFor="Location" mt={4}>
-            Meeting URL
-          </FormLabel>
-          <Input
-            id="Location"
-            {...register("event_location", {
-              required: true,
-            })}
-          />
-          <FormLabel mt={4}>Date</FormLabel>
-          <Controller
-            rules={{ required: true }}
-            control={control}
-            name="event_date"
-            required="true"
-            render={({ field }) => (
-              <DatePicker
-                dateFormat="MMMM d yyyy"
-                onChange={(e) => field.onChange(e)}
-                selected={field.value}
-              />
-            )}
-          />
+	return (
+		<>
+			<Box
+				py={4}
+				px={10}
+				// pl={10}
+				maxW="md"
+				borderWidth="1px"
+				borderRadius="lg"
+				overflow="hidden"
+				display="flex"
+				m="0 auto"
+				mt={10}
+				// align="center"
+			>
+				{/* event type */}
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<FormLabel htmlFor="type" mt={4}>
+						Event type
+					</FormLabel>
+					<Select
+						placeholder="Select the type of event"
+						{...register("event_type", {
+							required: true,
+						})}
+						className="event-type-selector"
+					>
+						<option value="Hackathon">Hackathon</option>
+						<option value="Code Club">Code Club</option>
+					</Select>
+					<FormLabel htmlFor="name" mt={4}>
+						Description
+					</FormLabel>
+					<Input
+						id="Description"
+						{...register("event_desc", {
+							required: true,
+						})}
+						className="event-desc-input"
+					/>
+					<FormLabel htmlFor="Location" mt={4}>
+						Meeting URL
+					</FormLabel>
+					<Input
+						id="Location"
+						{...register("event_location", {
+							required: true,
+						})}
+						className="meeting-url-input"
+					/>
+					<FormLabel mt={4}>Date</FormLabel>
+					<Controller
+						rules={{ required: true }}
+						control={control}
+						name="event_date"
+						required="true"
+						render={({ field }) => (
+							<DatePicker
+								dateFormat="MMMM d yyyy"
+								onChange={(e) => field.onChange(e)}
+								selected={field.value}
+								className="date-input"
+							/>
+						)}
+					/>
 
-          {/*start time range picker */}
-          <FormLabel mt={4}>Start Time</FormLabel>
+					{/*start time range picker */}
+					<FormLabel mt={4}>Start Time</FormLabel>
 
-          <Controller
-            rules={{ required: true }}
-            control={control}
-            name="event_start_time"
-            render={({ field }) => (
-              <DatePicker
-                selected={field.value}
-                onChange={(e) => field.onChange(e)}
-                showTimeSelect
-                showTimeSelectOnly
-                timeIntervals={30}
-                timeCaption="Time"
-                dateFormat="h:mm aa"
-              />
-            )}
-          />
+					<Controller
+						rules={{ required: true }}
+						control={control}
+						name="event_start_time"
+						render={({ field }) => (
+							<DatePicker
+								selected={field.value}
+								onChange={(e) => field.onChange(e)}
+								showTimeSelect
+								showTimeSelectOnly
+								timeIntervals={30}
+								timeCaption="Time"
+								dateFormat="h:mm aa"
+								className="start-time-input"
+							/>
+						)}
+					/>
 
-          {/* end time range picker */}
-          <FormLabel mt={4}>End Time</FormLabel>
+					{/* end time range picker */}
+					<FormLabel mt={4}>End Time</FormLabel>
 
-          <Controller
-            rules={{ required: true }}
-            control={control}
-            name="event_end_time"
-            render={({ field }) => (
-              <DatePicker
-                selected={field.value}
-                onChange={(e) => field.onChange(e)}
-                showTimeSelect
-                showTimeSelectOnly
-                timeIntervals={30}
-                timeCaption="Time"
-                dateFormat="h:mm aa"
-              />
-            )}
-          />
+					<Controller
+						rules={{ required: true }}
+						control={control}
+						name="event_end_time"
+						render={({ field }) => (
+							<DatePicker
+								selected={field.value}
+								onChange={(e) => field.onChange(e)}
+								showTimeSelect
+								showTimeSelectOnly
+								timeIntervals={30}
+								timeCaption="Time"
+								dateFormat="h:mm aa"
+								className="end-time-input"
+							/>
+						)}
+					/>
 
-          {/* tags  */}
-          <FormLabel mt={2}>
-            <EditIcon /> Provide three tags to help describe your event
-          </FormLabel>
-          <Editable width="200px" mt={2} placeholder="Tag 1">
-            <EditablePreview />
-            <EditableInput {...register("event_tags.0")} />
-          </Editable>
-          <Editable placeholder="Tag 2" width="200px" mt={2}>
-            <EditablePreview />
-            <EditableInput {...register("event_tags.1")} />
-          </Editable>
-          <Editable placeholder="Tag 3" width="200px" mt={2}>
-            <EditablePreview />
-            <EditableInput {...register("event_tags.2")} />
-          </Editable>
-          <Button width="200px" mt={4} type="submit" isLoading={isSubmitting}>
-            Submit
-          </Button>
-        </form>
-      </Box>
-      {formValues ? (
-        <BasicModal
-          isOpen={isOpen}
-          onClose={onClose}
-          button1="Edit details"
-          button2="Confirm event"
-          onClick={handleModalSubmit}
-          event_date={formValues.event_date}
-          event_type={formValues.event_type}
-          event_desc={formValues.event_desc}
-          event_start_time={formValues.event_start_time}
-          event_end_time={formValues.event_end_time}
-          event_location={formValues.event_location}
-          event_tags={formValues.event_tags}
-        />
-      ) : (
-        <></>
-      )}
-    </>
-  );
+					{/* tags  */}
+					<FormLabel mt={2}>
+						<EditIcon /> Provide three tags to help describe your event
+					</FormLabel>
+					<Editable width="200px" mt={2} placeholder="Tag 1">
+						<EditablePreview />
+						<EditableInput
+							{...register("event_tags.0")}
+							className="tag-1-input"
+						/>
+					</Editable>
+					<Editable
+						placeholder="Tag 2"
+						width="200px"
+						mt={2}
+						className="tag-2-input"
+					>
+						<EditablePreview />
+						<EditableInput {...register("event_tags.1")} />
+					</Editable>
+					<Editable placeholder="Tag 3" width="200px" mt={2}>
+						<EditablePreview className="tag-3-input" />
+						<EditableInput {...register("event_tags.2")} />
+					</Editable>
+					<Button
+						width="200px"
+						mt={4}
+						type="submit"
+						isLoading={isSubmitting}
+						className="submit-event-btn"
+					>
+						Submit
+					</Button>
+				</form>
+			</Box>
+			{formValues ? (
+				<BasicModal
+					isOpen={isOpen}
+					onClose={onClose}
+					button1="Edit details"
+					button2="Confirm event"
+					onClick={handleModalSubmit}
+					event_date={formValues.event_date}
+					event_type={formValues.event_type}
+					event_desc={formValues.event_desc}
+					event_start_time={formValues.event_start_time}
+					event_end_time={formValues.event_end_time}
+					event_location={formValues.event_location}
+					event_tags={formValues.event_tags}
+				/>
+			) : (
+				<></>
+			)}
+		</>
+	);
 }
 
 // limit description length (discuss length)
--- components/navbar.js
+++ components/navbar.js
@@ -1,149 +1,149 @@
 import {
-  Box,
-  Flex,
-  Avatar,
-  HStack,
-  IconButton,
-  Button,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuDivider,
-  useDisclosure,
-  useColorModeValue,
-  Stack,
+	Box,
+	Flex,
+	Avatar,
+	HStack,
+	IconButton,
+	Button,
+	Menu,
+	MenuButton,
+	MenuList,
+	MenuItem,
+	MenuDivider,
+	useDisclosure,
+	useColorModeValue,
+	Stack,
 } from "@chakra-ui/react";
 import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 import { useUser } from "@auth0/nextjs-auth0";
 import Link from "next/link";
 
 const Links = [
-  ["Create an event", "/api/auth/login"],
-  ["See all Events", "/events"],
+	["Create an event", "/api/auth/login"],
+	["See all Events", "/events"],
 ];
 const UserLinks = [
-  ["Create an event", "/create"],
-  ["See all Events", "/events"],
+	["Create an event", "/create"],
+	["See all Events", "/events"],
 ];
 const NavLink = ({ children }) => (
-  <Box
-    px={2}
-    py={1}
-    rounded={"md"}
-    _hover={{
-      textDecoration: "none",
-      color: "brand.primaryDark",
-    }}
-    color="brand.mainPurple"
-    fontWeight="bold"
-  >
-    <Link href={children[1]}>
-      <a>{children[0]}</a>
-    </Link>
-  </Box>
+	<Box
+		px={2}
+		py={1}
+		rounded={"md"}
+		_hover={{
+			textDecoration: "none",
+			color: "brand.primaryDark",
+		}}
+		color="brand.mainPurple"
+		fontWeight="bold"
+	>
+		<Link href={children[1]}>
+			<a>{children[0]}</a>
+		</Link>
+	</Box>
 );
 
 export default function Navbar() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const { user } = useUser();
-  return (
-    <Box fontFamily="Quicksand" px={4}>
-      <Flex
-        minH={"60px"}
-        py={{ base: 2 }}
-        px={{ base: 4 }}
-        borderBottom={1.5}
-        borderStyle={"solid"}
-        borderColor={useColorModeValue("gray.200", "gray.900")}
-        justifyContent={"space-between"}
-      >
-        <IconButton
-          size={"md"}
-          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-          aria-label={"Open Menu"}
-          display={{ md: "none" }}
-          onClick={isOpen ? onClose : onOpen}
-        />
-        <HStack spacing={8} alignItems={"center"}>
-          <Link href="/">
-            <Box cursor={"pointer"} fontSize="xl">
-              Co:llab
-            </Box>
-          </Link>
-          <HStack as={"nav"} spacing={4} display={{ base: "none", md: "flex" }}>
-            {user
-              ? UserLinks.map((link) => <NavLink key={link}>{link}</NavLink>)
-              : Links.map((link) => <NavLink key={link}>{link}</NavLink>)}
-          </HStack>
-        </HStack>
-        <Flex alignItems={"center"}>
-          {user ? (
-            <Menu>
-              <MenuButton
-                as={Button}
-                rounded={"full"}
-                variant={"link"}
-                cursor={"pointer"}
-                minW={0}
-              >
-                <Avatar
-                  size={"sm"}
-                  src={
-                    "https://lh3.googleusercontent.com/a/AATXAJwg6hNxXw0Ir5vsCmcNT5Wq3z_RcN3RglEYFSWs=s96-c"
-                  }
-                />
-              </MenuButton>
-              <MenuList>
-                <MenuItem>
-                  <Link href="/profile">
-                    <a>My events</a>
-                  </Link>
-                </MenuItem>
-                <MenuItem>
-                  <Link href="/api/auth/logout">
-                    <a>Organised events</a>
-                  </Link>
-                </MenuItem>
-                <MenuItem>
-                  <Link href="/api/auth/logout">
-                    <a>Past events</a>
-                  </Link>
-                </MenuItem>
-                <MenuItem>
-                  <Link href="/api/auth/logout">
-                    <a>Log out</a>
-                  </Link>
-                </MenuItem>
-              </MenuList>
-            </Menu>
-          ) : (
-            <Button
-              fontSize={"sm"}
-              fontWeight={600}
-              color={"white"}
-              bg={"brand.mainPurple"}
-              _hover={{
-                bg: "brand.primaryDark",
-              }}
-            >
-              <Link href="/api/auth/login">
-                <a>Sign up/Log in</a>
-              </Link>
-            </Button>
-          )}
-        </Flex>
-      </Flex>
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	const { user } = useUser();
+	return (
+		<Box fontFamily="Quicksand" px={4}>
+			<Flex
+				minH={"60px"}
+				py={{ base: 2 }}
+				px={{ base: 4 }}
+				borderBottom={1.5}
+				borderStyle={"solid"}
+				borderColor={useColorModeValue("gray.200", "gray.900")}
+				justifyContent={"space-between"}
+			>
+				<IconButton
+					size={"md"}
+					icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+					aria-label={"Open Menu"}
+					display={{ md: "none" }}
+					onClick={isOpen ? onClose : onOpen}
+				/>
+				<HStack spacing={8} alignItems={"center"}>
+					<Link href="/">
+						<Box cursor={"pointer"} fontSize="xl">
+							Co:llab
+						</Box>
+					</Link>
+					<HStack as={"nav"} spacing={4} display={{ base: "none", md: "flex" }}>
+						{user
+							? UserLinks.map((link) => <NavLink key={link}>{link}</NavLink>)
+							: Links.map((link) => <NavLink key={link}>{link}</NavLink>)}
+					</HStack>
+				</HStack>
+				<Flex alignItems={"center"}>
+					{user ? (
+						<Menu>
+							<MenuButton
+								as={Button}
+								rounded={"full"}
+								variant={"link"}
+								cursor={"pointer"}
+								minW={0}
+							>
+								<Avatar
+									size={"sm"}
+									src={
+										"https://lh3.googleusercontent.com/a/AATXAJwg6hNxXw0Ir5vsCmcNT5Wq3z_RcN3RglEYFSWs=s96-c"
+									}
+								/>
+							</MenuButton>
+							<MenuList>
+								<MenuItem>
+									<Link href="/profile">
+										<a>My events</a>
+									</Link>
+								</MenuItem>
+								<MenuItem>
+									<Link href="/api/auth/logout">
+										<a>Organised events</a>
+									</Link>
+								</MenuItem>
+								<MenuItem>
+									<Link href="/api/auth/logout">
+										<a>Past events</a>
+									</Link>
+								</MenuItem>
+								<MenuItem>
+									<Link href="/api/auth/logout">
+										<a>Log out</a>
+									</Link>
+								</MenuItem>
+							</MenuList>
+						</Menu>
+					) : (
+						<Button
+							fontSize={"sm"}
+							fontWeight={600}
+							color={"white"}
+							bg={"brand.mainPurple"}
+							_hover={{
+								bg: "brand.primaryDark",
+							}}
+						>
+							<Link href="/api/auth/login">
+								<a className="log-in-button">Sign up/Log in</a>
+							</Link>
+						</Button>
+					)}
+				</Flex>
+			</Flex>
 
-      {isOpen ? (
-        <Box pb={4} display={{ md: "none" }}>
-          <Stack as={"nav"} spacing={3} pt={3}>
-            {user
-              ? UserLinks.map((link) => <NavLink key={link}>{link}</NavLink>)
-              : Links.map((link) => <NavLink key={link}>{link}</NavLink>)}
-          </Stack>
-        </Box>
-      ) : null}
-    </Box>
-  );
+			{isOpen ? (
+				<Box pb={4} display={{ md: "none" }}>
+					<Stack as={"nav"} spacing={3} pt={3}>
+						{user
+							? UserLinks.map((link) => <NavLink key={link}>{link}</NavLink>)
+							: Links.map((link) => <NavLink key={link}>{link}</NavLink>)}
+					</Stack>
+				</Box>
+			) : null}
+		</Box>
+	);
 }
