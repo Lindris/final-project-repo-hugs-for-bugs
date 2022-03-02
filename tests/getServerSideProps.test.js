12bbc404644ad17bd3d0294816dc10d1be6dcba2
Hajara <hajaraiyal@gmail.com>
2022-03-02 15:10:23 +0000

    jest test written to get all events

---
+++ tests/getServerSideProps.test.js
@@ -0,0 +1,24 @@
+import { getServerSideProps } from "../pages/events";
+import fetch from "node-fetch";
+
+window.fetch = fetch;
+
+describe("Check events", () => {
+	test("check if we get a specific event", async () => {
+		const actual = await getServerSideProps();
+
+		const actualData = actual.props.payload;
+		const expected = {
+			event_id: expect.any(Number),
+			event_desc: expect.any(String),
+			event_date: expect.any(String),
+			event_start_time: expect.any(String),
+			event_end_time: expect.any(String),
+			event_location: expect.any(String),
+			event_type: expect.any(String),
+			event_tags: expect.any(Array),
+			auth_id: expect.any(String),
+		};
+		expect(actualData).toContainEqual(expected);
+	});
+});
