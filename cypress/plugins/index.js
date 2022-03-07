7c5b27ba87dec35b7d182fb5989d14f88b48ce03
Hajara <hajaraiyal@gmail.com>
2022-03-07 13:48:13 +0000

    testing auth

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,7 +16,12 @@
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
+// module.exports = (on, config) => {
+// `on` is used to hook into various events Cypress emits
+// `config` is the resolved Cypress config}
+
+const encrypt = require("cypress-nextjs-auth0/encrypt");
+
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
+	on("task", { encrypt });
+};
